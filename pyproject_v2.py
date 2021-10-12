b85886e5a08b1b69d163b8780cdded9c31f00d56
Jennie <jenniferdacostazilli@hotmail.com>
2021-10-12 17:19:56 -0300

    1210v1

---
+++ pyproject_v2.py
@@ -0,0 +1,9 @@
+import math
+
+raio = input('Raio: ')
+
+print('Área do Circulo', math.pi * float(raio) ** 2)
+
+# ** potencia
+# ctrl ' abre e fecha terminal
+# cd fundpy entra na pasta, python pyproject.py roda o código
---
+++ pyproject_v3.py
@@ -0,0 +1,12 @@
+import math
+
+raio = input('Raio: ')
+
+print('Área do Circulo', math.pi * float(raio) ** 2)
+
+print("Nome do módulo", __name__)
+print('Nome do pacote', __package__)
+
+# ** potencia
+# ctrl ' abre e fecha terminal
+# cd fundpy entra na pasta, python pyproject.py roda o código
